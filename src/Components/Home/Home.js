9bed83468378d737db4fbd3fa9727b67787209fe
Vasundraa <vasundraas@gmail.com>
2024-04-23 14:39:31 +0530

    new commit home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 import Hero from '../Hero/Hero';
 import Menus from '../Menus/Menus';
 import Title from '../Title/Title';
-import About from '../About/About';
+//import About from '../About/About';
 
 import Contact from '../Contact Us/Contact';
 import Footer from '../Footer/Footer';
 import VideoPlayer from '../VideoPlayer/VideoPlayer';
-import Profile from '../Profile/Profile';
+//import Profile from '../Profile/Profile';
 
 const Home = () => {
   const [playState, setPlayState] = useState(false);
