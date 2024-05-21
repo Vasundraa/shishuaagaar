76bb9c08b907de40de4e7c2ce646667346ca03b3
Vasundraa <150444680+Vasundraa@users.noreply.github.com>
2024-05-21 20:49:57 +0530

    Update Poopinfo.js

--- src/Components/showProduct/Poopinfo.js
+++ src/Components/showProduct/Poopinfo.js
@@ -6,7 +6,7 @@ const Poopinfo = () => {
 
     const getPoops = async () => {
         try {
-            const response = await axios.get('https://sisuagaar-data.onrender.com/poop_detail/');
+            const response = await axios.get('https://sisu-hfzm.onrender.com/poop_detail/');
             const poopsWithData = response.data.map(poop => ({
                 ...poop,
                 parentName: poop.parentName, // Fetch parent name
